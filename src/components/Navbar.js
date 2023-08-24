e534512f7fb3577766436e5b937cfc3cace0002f
Jen Brannstrom <jenbrannstrom@users.noreply.github.com>
2023-08-25 01:27:52 +0300

    edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { AppBar, Toolbar, Typography, Box } from '@mui/material';
-import { makeStyles } from '@mui/material';
 import LanguageSwitcher from './LanguageSwitcher';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
 const Navbar = () => {
-  const classes = useStyles();
   const { t } = useTranslation();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             {t('appName')}
           </Typography>
           <LanguageSwitcher />
